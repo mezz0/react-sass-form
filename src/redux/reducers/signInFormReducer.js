4a313ecf822468ddd20c82191af35d7af9e42439
Mehul J <mehul.jadeja@fdj-gs.com>
2021-05-10 08:00:11 +0100

    remove unused packages

--- src/redux/reducers/signInFormReducer.js
+++ src/redux/reducers/signInFormReducer.js
@@ -4,25 +4,15 @@ const initialState = {
 };
 
 const userReducer = (state = initialState, action) => {
-  let user = state.user;
-
   switch (action.type) {
     case "SIGN_IN":
-      user.push(action.payload);
-
       return {
-        ...state,
-        user,
         signedIn: true
       };
 
     case "SIGN_OUT":
       return {
-        ...state,
-        user: user.filter(
-          (item) => item.email !== action.payload.email
-        ),
-        signedIn: true
+        signedIn: false
       };
 
     default:
